0648a21601336aaafb947bdc8ef828d7cc79ccc6
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-03-05 20:37:29 +0600

    commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,34 @@
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import CartProduct from "../CartProduct/CartProduct";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
 
 const Cart = () => {
-	const cart = useSelector(state => state.cart);
-return (
-	<div>
-	{cart.length === 0 && <h1 className="text-center my-5">No Products Added</h1>}
-	  {
-	  	cart.map(product => <CartProduct product={product} key={product.id}/>)
-	  }
-	  <Button as={Link} to="/" size="sm" className="my-3">
-	 <FontAwesomeIcon icon={faAdd}/> Add product 
-	  </Button>
-	  <Button as={Link} to="/pricing" variant="danger" className="d-block w-75 mx-auto my-3 ">Confirm Order</Button>
-	</div>
-);
+  const cart = useSelector((state) => state.cart);
+  return (
+    <div>
+      {cart.length === 0 && (
+        <h1 className="text-center my-5">No Products Added</h1>
+      )}
+      {cart.map((product) => (
+        <CartProduct product={product} key={product.id} />
+      ))}
+      <Button as={Link} to="/" size="sm" className="my-3">
+        <FontAwesomeIcon icon={faAdd} /> Add product
+      </Button>
+      <Button
+        as={Link}
+        to="/pricing"
+        variant={cart.length ? "danger" : "secondary"}
+        disabled={cart.length ? false : true}
+        className="d-block w-75 mx-auto my-3 "
+      >
+        Confirm Order
+      </Button>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 const Pricing = () => {
   const price = useSelector((state) => state.price);
@@ -14,27 +14,40 @@ const Pricing = () => {
     { type: "TOTAL", value: total },
   ];
   const handlePay = () => {
-  	dispatch({ type: "CLEAN_CART" });
-  	navigate("/");
+    dispatch({ type: "CLEAN_CART" });
+    navigate("/");
   };
   return (
     <div className="">
       {allPricing.map((pricing, i) => (
         <p
-          className="d-flex justify-content-between bg-primary bg-opacity-25 p-3 rounded my-2"
+          className="d-flex justify-content-between bg-primary bg-opacity-25 p-3 rounded my-3"
           key={i}
         >
           <span>{pricing.type}</span>
           <span>${pricing.value}</span>
         </p>
       ))}
-      <Button
-        variant="danger"
-        className="d-block w-75 mx-auto my-3"
-        onClick={handlePay}
-      >
-        Pay Now
-      </Button>
+      <Form className="mt-5" onSubmit={handlePay}>
+        <Form.Control type="text" 
+        placeholder="Pickup Point" 
+        className="my-3"
+        required 
+        />
+        <Form.Control
+          as="textarea"
+          className="my-3"
+          placeholder="Add delivery instructions"
+          rows={2}
+        />
+        <Button
+          variant="danger" 
+          type="submit"
+          className="d-block w-75 mx-auto my-3"
+        >
+          Pay Now
+        </Button>
+      </Form>
     </div>
   );
 };
